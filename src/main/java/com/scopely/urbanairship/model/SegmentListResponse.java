454d1db99355d73619656a2d6f9491e89c255f80
Daniel Lamando <daniel.lamando@scopely.com>
2014-11-07 11:27:23 -0800

    Fix some annotations

--- src/main/java/com/scopely/urbanairship/model/SegmentListResponse.java
+++ src/main/java/com/scopely/urbanairship/model/SegmentListResponse.java
@@ -1,20 +1,19 @@
 package com.scopely.urbanairship.model;
 
 import com.scopely.urbanairship.api.Segments;
-import com.sun.istack.internal.Nullable;
 
 import java.net.URI;
 import java.util.List;
 
 public class SegmentListResponse {
-    @Nullable private URI nextPage;
+    private URI nextPage;
     private List<Segment> segments;
 
-    @Nullable public URI getNextPageUrl() {
+    public URI getNextPageUrl() {
         return nextPage;
     }
 
-    @Nullable public SegmentListResponse fetchNextPageUrl(Segments api) {
+    public SegmentListResponse fetchNextPageUrl(Segments api) {
         if (nextPage == null) {
             return null;
         }
